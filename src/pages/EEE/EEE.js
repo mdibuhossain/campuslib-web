c728310fc5bc2edb77dbe15ad6f9d1663d1d4306
Ibrahim H <mohammad.ihakash@gmail.com>
2022-05-02 06:00:12 +0600

    pages update

--- src/pages/EEE/EEE.js
+++ src/pages/EEE/EEE.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Accordionlist from '../../components/Accordionlist';
 import { Banner } from '../Home/Home.style';
 
 const EEE = () => {
     return (
         <>
             <Banner title="Electrical & Electronics Engineering" src="assets/images/eee.jpg" />
+            <div className='md:w-3/5 w-11/12 m-auto my-10 md:border-2 md:rounded-lg md:p-10'>
+                <Accordionlist title="Syllabus ↓" />
+                <Accordionlist title="Academic Books ↓" />
+                <Accordionlist title="Others Books ↓" />
+                <Accordionlist title="Previous Year Questions ↓" />
+            </div>
         </>
     );
 };
--- src/pages/MAT/MAT.js
+++ src/pages/MAT/MAT.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Accordionlist from '../../components/Accordionlist';
 import { Banner } from '../Home/Home.style';
 
 const MAT = () => {
     return (
         <>
             <Banner title="Mathematics" src="assets/images/math.jpg" />
+            <div className='md:w-3/5 w-11/12 m-auto my-10 md:border-2 md:rounded-lg md:p-10'>
+                <Accordionlist title="Syllabus ↓" />
+                <Accordionlist title="Academic Books ↓" />
+                <Accordionlist title="Others Books ↓" />
+                <Accordionlist title="Previous Year Questions ↓" />
+            </div>
         </>
     );
 };
