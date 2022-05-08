8c6dd682dd0f640804fca50b8eadf67fe2372c7a
Ibrahim H <mohammad.ihakash@gmail.com>
2022-05-08 07:16:53 +0600

    fix: design changed

--- src/components/BookShowcase.js
+++ src/components/BookShowcase.js
@@ -13,7 +13,7 @@ const BookShowcase = ({ department }) => {
     useBooks(setSyllabus, department, "syllabus");
 
     return (
-        <div className='md:w-3/5 w-11/12 m-auto my-10 md:border-2 md:rounded-lg md:p-10 bg-white'>
+        <div className='md:w-3/5 w-11/12 m-auto my-10 md:border-2 md:rounded-lg p-10 bg-white'>
             <Accordionlist title="Syllabus ↓" contents={syllabus} />
             <Accordionlist title="Academic Books ↓" contents={academic} />
             <Accordionlist title="Previous Year Questions ↓" contents={questions} />
