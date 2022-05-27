06222b6e7baa84e16d191bebdee2779bae6d2a1a
Ibrahim H <mohammad.ihakash@gmail.com>
2022-05-27 20:55:14 +0600

    test:Done

--- src/Hooks/useBooks.js
+++ src/Hooks/useBooks.js
@@ -1,16 +1,28 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
+import { useAuth } from "./useAuth"
 
 export const useBooks = (setData, department, cat) => {
+    const { books, questions, syllabus, allData } = useAuth();
     useEffect(() => {
-        fetch(process.env.REACT_APP_JSON_URL)
-            .then(res => res.json())
-            .then(data => {
-                const display_data = []
-                for (const info of data[cat]) {
-                    if (info?.categories?.includes(department))
-                        display_data.push(info)
-                }
-                setData(display_data)
-            })
-    }, [])
+        switch (cat) {
+            case "books":
+                const tmpBooks = books.filter(item => item.categories === department)
+                setData(tmpBooks)
+                break
+            case "questions":
+                const tmpQuestions = questions.filter(item => item.categories === department)
+                setData(tmpQuestions)
+                break
+            case "syllabus":
+                const tmpSyllabus = syllabus.filter(item => item.categories === department)
+                setData(tmpSyllabus)
+                break
+            case "all":
+                setData([...books, ...questions, ...syllabus])
+                break
+            default:
+                setData([])
+                break
+        }
+    }, [books, questions, syllabus, cat, setData, department])
 }
