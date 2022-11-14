dd56984a897390ce3150459b99710d2675e09307
Ibrahim Hossain <ibuhossain3@gmail.com>
2022-11-14 19:38:34 +0600

    update: error fixed

--- src/queries/query.js
+++ src/queries/query.js
@@ -78,16 +78,16 @@ const GET_ADMIN = gql`
 // post data
 const POST_BOOK = gql`
   mutation AddBook(
-    book_name: String
-    download_link: String
-    categories: String
-    sub_categories: String
-    added_by: String
-    status: Boolean
-    author: String
-    edition: Number
-    token: String
-  ){
+    $book_name: String
+    $download_link: String
+    $categories: String
+    $sub_categories: String
+    $added_by: String
+    $status: Boolean
+    $author: String
+    $edition: Number
+    $token: String
+  ) {
     addBook(
       book_name: $book_name
       download_link: $download_link
@@ -98,21 +98,21 @@ const POST_BOOK = gql`
       author: $author
       edition: $edition
       token: $token
-    ){
+    ) {
       _id
     }
   }
 `;
 const POST_QUESTION = gql`
   mutation AddQuestion(
-    book_name: String
-    download_link: String
-    categories: String
-    sub_categories: String
-    added_by: String
-    status: Boolean
-    token: String
-  ){
+    $book_name: String
+    $download_link: String
+    $categories: String
+    $sub_categories: String
+    $added_by: String
+    $status: Boolean
+    $token: String
+  ) {
     addQuestion(
       book_name: $book_name
       download_link: $download_link
@@ -121,21 +121,21 @@ const POST_QUESTION = gql`
       added_by: $added_by
       status: $status
       token: $token
-    ){
+    ) {
       _id
     }
   }
 `;
 const POST_SYLLABUS = gql`
   mutation AddSyllabus(
-    book_name: String
-    download_link: String
-    categories: String
-    sub_categories: String
-    added_by: String
-    status: Boolean
-    token: String
-  ){
+    $book_name: String
+    $download_link: String
+    $categories: String
+    $sub_categories: String
+    $added_by: String
+    $status: Boolean
+    $token: String
+  ) {
     addSyllabus(
       book_name: $book_name
       download_link: $download_link
@@ -144,7 +144,7 @@ const POST_SYLLABUS = gql`
       added_by: $added_by
       status: $status
       token: $token
-    ){
+    ) {
       _id
     }
   }
@@ -172,14 +172,14 @@ const POST_USER = gql`
 // update data
 const UPDATE_BOOK = gql`
   mutation EditBook(
-    book_name: String
-    download_link: String
-    categories: String
-    sub_categories: String
-    author: String
-    edition: Number
-    token: String
-  ){
+    $book_name: String
+    $download_link: String
+    $categories: String
+    $sub_categories: String
+    $author: String
+    $edition: Number
+    $token: String
+  ) {
     editBook(
       book_name: $book_name
       download_link: $download_link
@@ -188,45 +188,45 @@ const UPDATE_BOOK = gql`
       author: $author
       edition: $edition
       token: $token
-    ){
+    ) {
       _id
     }
   }
 `;
 const UPDATE_QUESTION = gql`
   mutation EditQuestion(
-    book_name: String
-    download_link: String
-    categories: String
-    sub_categories: String
-    token: String
-  ){
+    $book_name: String
+    $download_link: String
+    $categories: String
+    $sub_categories: String
+    $token: String
+  ) {
     editQuestion(
       book_name: $book_name
       download_link: $download_link
       categories: $categories
       sub_categories: $sub_categories
       token: $token
-    ){
+    ) {
       _id
     }
   }
 `;
 const UPDATE_SYLLABUS = gql`
   mutation EditSyllabus(
-    book_name: String
-    download_link: String
-    categories: String
-    sub_categories: String
-    token: String
-  ){
+    $book_name: String
+    $download_link: String
+    $categories: String
+    $sub_categories: String
+    $token: String
+  ) {
     editSyllabus(
       book_name: $book_name
       download_link: $download_link
       categories: $categories
       sub_categories: $sub_categories
       token: $token
-    ){
+    ) {
       _id
     }
   }
@@ -248,22 +248,22 @@ const MAKE_ADMIN = gql`
 
 // delete data
 const DELETE_BOOK = gql`
-  mutation DeleteBook(_id: ID,token: String){
-    deleteBook(_id: $_id,token: $token){
+  mutation DeleteBook($_id: ID, $token: String) {
+    deleteBook(_id: $_id, token: $token) {
       _id
     }
   }
 `;
 const DELETE_QUESTION = gql`
-  mutation DeleteQuestion(_id: ID,token: String){
-    deleteQuestion(_id: $_id,token: $token){
+  mutation DeleteQuestion($_id: ID, $token: String) {
+    deleteQuestion(_id: $_id, token: $token) {
       _id
     }
   }
 `;
 const DELETE_SYLLABUS = gql`
-  mutation DeleteSyllabus(_id: ID,token: String){
-    deleteSyllabus(_id: $_id,token: $token){
+  mutation DeleteSyllabus($_id: ID, $token: String) {
+    deleteSyllabus(_id: $_id, token: $token) {
       _id
     }
   }
