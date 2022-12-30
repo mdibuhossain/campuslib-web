1e02e6c8617b6dd7af782e351ce1e3f8fce41adf
chadnipodder <chadnipodder@gmail.com>
2022-12-31 01:24:17 +0600

    update: profile picture same file select issue fixed

--- src/pages/ChangeDP/ChangeDP.js
+++ src/pages/ChangeDP/ChangeDP.js
@@ -15,13 +15,17 @@ const ChangeDP = () => {
         setShowModal(false)
         setSelectedImg(null)
         setScale(1)
-        setRotate(0);
+        setRotate(0)
     }
     const handleSelectImg = (e) => {
         const img = e.target.files[0]
-        setSelectedImg(img);
-        if (img !== null)
+        if (img !== null) {
+            setSelectedImg(img)
             setShowModal(true)
+        }
+    }
+    function handleWhileSelectImg(e) {
+        e.target.value = null
     }
 
     const showCroppedImage = async () => {
@@ -35,7 +39,7 @@ const ChangeDP = () => {
     return (
         <PageLayout>
             <div className="relative">
-                <Typography variant='h5' sx={{ fontWeight: 600, textAlign: 'center', my: 4 }}>CHANGE PROFILE PICTURE</Typography>
+                <Typography variant='h5' sx={{ fontWeight: 600, textAlign: 'center', my: 4 }}>Profile settings</Typography>
                 <div className="text-center flex justify-center items-center flex-col my-20">
                     <dir className="relative p-0">
                         <img className="w-40 rounded-full" src={user.photoURL} alt="profile" />
@@ -48,7 +52,7 @@ const ChangeDP = () => {
                                         <path d="M10.5 8.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
                                         <path d="M2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2zm.5 2a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm9 2.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z" />
                                     </svg>
-                                    <input onChange={handleSelectImg} accept="image/*" type="file" id="avatarSelect" className="hidden" />
+                                    <input onChange={handleSelectImg} onClick={handleWhileSelectImg} accept="image/*" type="file" id="avatarSelect" className="hidden" />
                                 </label>
                             </div>
                         </div>
@@ -103,10 +107,10 @@ const ChangeDP = () => {
                                                     className="form-range w-full h-6 mx-2 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none"
                                                     min="1"
                                                     max="10"
-                                                    defaultValue='1'
+                                                    defaultValue={1}
                                                     step="0.01"
                                                     id="customRange3"
-                                                    onChange={(e) => setScale(e.target.value)}
+                                                    onChange={(e) => setScale(Number(e.target.value))}
                                                 />
                                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM10 7v3m0 0v3m0-3h3m-3 0H7" />
@@ -121,10 +125,10 @@ const ChangeDP = () => {
                                                     className="form-range w-full h-6 mx-2 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none"
                                                     min="0"
                                                     max="360"
-                                                    defaultValue='0'
+                                                    defaultValue={0}
                                                     step="0.5"
                                                     id="customRange3"
-                                                    onChange={(e) => setRotate(e.target.value)}
+                                                    onChange={(e) => setRotate(Number(e.target.value))}
                                                 />
                                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" style={{ transform: 'scaleX(-1)' }}>
                                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 10h10a8 8 0 018 8v2M3 10l6 6m-6-6l6-6" />
