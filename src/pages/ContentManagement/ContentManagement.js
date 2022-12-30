9723d7c5d7189d383807e5222b920946dd599965
chadnipodder <chadnipodder@gmail.com>
2022-12-31 00:33:05 +0600

    update: content management selection tab extra added to the bottom

--- src/pages/ContentManagement/ContentManagement.js
+++ src/pages/ContentManagement/ContentManagement.js
@@ -23,21 +23,25 @@ const TabActive = () => {
     }
 }
 
+const TabSection = ({ activeHandler, _isActive }) => (
+    <section className="px-2 my-10">
+        <nav className="flex justify-center w-full md:w-5/12 m-auto p-0">
+            <Button name="_books" onClick={activeHandler} variant="outlined" style={{ borderRadius: '0px', borderTopLeftRadius: "0.375rem", borderBottomLeftRadius: "0.375rem", color: "inherit", backgroundColor: _isActive["_books"] ? "rgb(56 189 248)" : "inherit" }} className="flex-1">BOOKS</Button>
+
+            <Button name="_questions" onClick={activeHandler} variant="outlined" style={{ borderRadius: '0px', color: "inherit", backgroundColor: _isActive["_questions"] ? "rgb(56 189 248)" : "inherit" }} className="flex-1">QUESTIONS</Button>
+
+            <Button name="_syllabus" onClick={activeHandler} variant="outlined" style={{ borderRadius: '0px', borderTopRightRadius: "0.375rem", borderBottomRightRadius: "0.375rem", color: "inherit", backgroundColor: _isActive["_syllabus"] ? "rgb(56 189 248)" : "inherit" }} className="flex-1">SYLLABUS</Button>
+        </nav>
+    </section>
+)
+
 const ContentManagement = ({ isMyContent = false, pageTitle }) => {
     const { _isActive, activeHandler } = TabActive();
     const { books, questions, syllabus } = useUtility()
     return (
         <PageLayout>
             <Typography variant='h5' sx={{ fontWeight: 600, textAlign: 'center', my: 4 }}>{pageTitle}</Typography>
-            <section className="px-2 mb-10">
-                <nav className="flex justify-center w-full md:w-5/12 m-auto p-0">
-                    <Button name="_books" onClick={activeHandler} variant="outlined" style={{ borderRadius: '0px', borderTopLeftRadius: "0.375rem", borderBottomLeftRadius: "0.375rem", color: "inherit", backgroundColor: _isActive["_books"] ? "rgb(56 189 248)" : "inherit" }} className="flex-1">BOOKS</Button>
-
-                    <Button name="_questions" onClick={activeHandler} variant="outlined" style={{ borderRadius: '0px', color: "inherit", backgroundColor: _isActive["_questions"] ? "rgb(56 189 248)" : "inherit" }} className="flex-1">QUESTIONS</Button>
-
-                    <Button name="_syllabus" onClick={activeHandler} variant="outlined" style={{ borderRadius: '0px', borderTopRightRadius: "0.375rem", borderBottomRightRadius: "0.375rem", color: "inherit", backgroundColor: _isActive["_syllabus"] ? "rgb(56 189 248)" : "inherit" }} className="flex-1">SYLLABUS</Button>
-                </nav>
-            </section>
+            <TabSection _isActive={_isActive} activeHandler={activeHandler} />
             {
                 _isActive["_books"] && <ListOfRequest content={books} status={isMyContent} title="Book" />
             }
@@ -47,6 +51,7 @@ const ContentManagement = ({ isMyContent = false, pageTitle }) => {
             {
                 _isActive["_syllabus"] && <ListOfRequest content={syllabus} status={isMyContent} title="Syllabus" />
             }
+            <TabSection _isActive={_isActive} activeHandler={activeHandler} />
         </PageLayout>
     );
 };
