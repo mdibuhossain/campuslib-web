771235cfdb4a1bd7e989d1e5830b4225dec0f7a7
Md. Ibrahim Hossain <ibuhossain3@gmail.com>
2024-11-08 03:13:41 +0600

    updated'

--- src/pages/Home/Home.style.jsx
+++ src/pages/Home/Home.style.jsx
@@ -30,7 +30,7 @@ const Banner = styled.section`
         transform: translate(-50%,-50%);
         border: 2px solid white;
         padding: 0.75em 1.75em;
-        -webkit-mask-image: linear-gradient(-75deg, rgba(0,0,0,.75) 30%, #000 50%, rgba(0,0,0,.75) 70%);
+        -webkit-mask-image: linear-gradient(-75deg, rgba(0,0,0,.75) 40%, #000 50%, rgba(0,0,0,.75) 70%);
         -webkit-mask-size: 200%;
         animation: shine 1.85s linear infinite;
         @keyframes shine {
