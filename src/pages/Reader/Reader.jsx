037851aaa00f95bacfe1919b5a6683f1c78bbfde
Ibrahim <ibuhossain3@gmail.com>
2025-01-12 03:23:10 +0600

    update

--- src/pages/Reader/Reader.jsx
+++ src/pages/Reader/Reader.jsx
@@ -4,6 +4,7 @@ import PageLayout from "../../Layout/PageLayout";
 function Reader() {
   const [isDragging, setIsDragging] = useState(false);
   const [file, setFile] = useState(null);
+  const [previewContent, setPreviewContent] = useState(null);
 
   const handleDragEnter = (e) => {
     e.preventDefault();
@@ -34,12 +35,74 @@ function Reader() {
 
   const handleFileSelect = (e) => {
     if (e.target.files && e.target.files.length > 0) {
-      setFile(e.target.files[0]);
+      const selectedFile = e.target.files[0];
+      setFile(selectedFile);
+      generatePreview(selectedFile);
+    }
+  };
+
+  const generatePreview = (file) => {
+    console.log(file);
+    const reader = new FileReader();
+    const fileType = file.type;
+    if (fileType.startsWith("image")) {
+      reader.onload = (e) =>
+        setPreviewContent(
+          <img
+            src={e.target.result}
+            alt="Preview"
+            className="max-w-full max-h-96"
+          />
+        );
+      reader.readAsDataURL(file);
+    } else if (fileType === "application/pdf") {
+      reader.onload = (e) => {
+        setPreviewContent(
+          <iframe
+            src={e.target.result}
+            title="PDF Preview"
+            className="w-full h-full"
+          />
+        );
+      };
+      reader.readAsDataURL(file);
+    } else if (
+      [
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        "application/msword",
+      ].includes(fileType)
+    ) {
+      import("mammoth").then((mammoth) => {
+        reader.onload = async (e) => {
+          const result = await mammoth.extractRawText({
+            arrayBuffer: e.target.result,
+          });
+          setPreviewContent(
+            <div className="whitespace-pre-wrap text-sm">{result.value}</div>
+          );
+        };
+        reader.readAsArrayBuffer(file);
+      });
+    } else if (
+      fileType ===
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    ) {
+      import("xlsx").then((xlsx) => {
+        reader.onload = (e) => {
+          const workBook = xlsx.read(e.target.result, { type: "array" });
+          const sheetName = workBook.SheetNames[0];
+          const sheet = workBook.Sheets[sheetName];
+          const sheetData = xlsx.utils.sheet_to_json(sheet, { header: 1 });
+          setPreviewContent(<div dangerouslySetInnerHTML={{ __html: sheetData }} />);
+        };
+        reader.readAsArrayBuffer(file);
+      });
     }
   };
 
   return (
     <PageLayout>
+      {previewContent}
       <div className="flex flex-col items-center justify-center h-full">
         <div
           className={`border-4 border-dashed p-10 mx-5 rounded-lg transition-all duration-300 ${
